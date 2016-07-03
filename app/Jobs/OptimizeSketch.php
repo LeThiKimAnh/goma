aac4de3a3223167686ee1ceeb8ff6242d7389efc
simpletrontdip <thongitbk@gmail.com>
2016-07-03 19:32:50 +0700

    change default order_id to received value

--- app/Jobs/OptimizeSketch.php
+++ app/Jobs/OptimizeSketch.php
@@ -32,7 +32,7 @@ class OptimizeSketch extends Job implements ShouldQueue {
 	 */
 	public function handle() {
 		Log::info("Starting optimze the session");
-		$order_id = 2; #$this->session->donhang_id;
+		$order_id = $this->session->donhang_id;
 		$gothua = DB::select("SELECT dai as height,rong as width, chat_lieu as req FROM goma.go_thua;");
 		$sql = "SELECT c.ten as `code`, SUM(a.so_luong * b.so_luong) as count, c.dai as height, c.rong as width, c.yeu_cau as req FROM chi_tiet_vat_dung as a inner join chi_tiet_don_hang as b ON a.vatdung_id = b.vatdung_id and b.donhang_id = $order_id inner join vat_lieu as c ON a.vatlieu_id = c.id GROUP BY a.vatlieu_id;";
 		$res = DB::select($sql);
