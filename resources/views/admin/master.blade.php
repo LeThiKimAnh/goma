3c9b5cc4fb2c4157ff51fc417ed2776a77c48676
LeThiKimAnh <kimanhitbk@gmail.com>
2016-07-24 12:30:15 +0700

    them logo -_-

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -55,6 +55,12 @@
 			.glyphicon-bed:before {
 			  	content: "\e219";
 			}
+			.navbar {
+				min-height: 65px;
+			}
+			.sidebar{
+				margin-top: 65px;
+			}
 		</style>
 	</head>
 
@@ -70,8 +76,11 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="{!!URL::route('dashboard')!!}">Admin Area - {!!Auth::user()->username!!}</a>
+					<a href="{!!URL::route('dashboard')!!}">
+						<img src="{{url('admin/images/logo.png')}}" width="70px" height="50px" style="margin-left:80px; margin-top:5px">
+					</a>	
 				</div>
+				<div class="navbar-brand " style="margin-left:375px;"><h2 style="margin-top: 0px;">Công ty TNHH Tuyết Nga</h2></div>
 				<!-- /.navbar-header -->
 
 				<ul class="nav navbar-top-links navbar-right">
@@ -99,7 +108,7 @@
 				<div class="navbar-default sidebar" role="navigation">
 					<div class="sidebar-nav navbar-collapse">
 						<ul class="nav" id="side-menu">
-							<li class="sidebar-search">
+							<!-- <li class="sidebar-search">
 								<div class="input-group custom-search-form">
 									<input type="text" class="form-control" placeholder="Search...">
 									<span class="input-group-btn">
@@ -108,8 +117,7 @@
 										</button>
 									</span>
 								</div>
-								<!-- /input-group -->
-							</li>
+							</li> -->
 							<li>
 								<a href="{!!URL::route('dashboard')!!}"><i class="glyphicon glyphicon-dashboard"></i> Dashboard</a>
 							</li>
@@ -196,7 +204,7 @@
 				<div class="container-fluid">
 					<!-- Page Heading -->
 					<div class="row">
-						<div class="col-lg-12" style="margin-top:20px; ">
+						<div class="col-lg-12" style="margin-top:40px; ">
 							<h2 class="page-header">@yield('controller')
 								<small>@yield('action')</small>
 							</h2>
